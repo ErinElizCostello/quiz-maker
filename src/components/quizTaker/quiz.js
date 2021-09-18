15d97c5945990ea5bb1d6111dccb11c94f2fec53
Erin <erin.eliz.costello@gmail.com>
2021-09-18 16:43:48 -0600

    created outline for quiz taking page, displays questions and answers

--- src/components/quizTaker/quiz.js
+++ src/components/quizTaker/quiz.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 
 
 const Quiz = () => {
 
-  
+  const theQuizID = useSelector(state => state.setQuizID)
 
-  return ( 
+  const [theQuiz, setTheQuiz] = useState(null)
+
+  useEffect(() => {
+    theQuizID &&
+      fetch(`http://localhost:3001/quiz/${theQuizID}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log('data', data)
+          setTheQuiz(data.data)
+        })
+
+
+  }, []);
+
+  return (
     <div>
-      heres the quiz
+      <div>
+        {theQuiz && theQuiz.title}
+      </div>
+      <div>
+        {theQuiz && theQuiz.quiz.map(thisQuiz => (
+          <div style={{margin: 20}}>
+            <div>
+              {thisQuiz.question}
+            </div>
+            <div style={{margin: 10}}></div>
+            <div>
+              {thisQuiz.answers.map(answer => (
+                <div>
+                  {`${answer.letter}. ${answer.text}`}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default Quiz;
