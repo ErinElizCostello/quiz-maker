b16b8fff29100edc94bdbda754b861e31c779662
Erin <erin.eliz.costello@gmail.com>
2021-12-21 00:38:15 -0700

    Version 1.0.0

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 const db = require('./db')
 const router = require('./routes/routes')
+const path = require('path')
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -14,6 +15,13 @@ app.use(cors())
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use('/', router)
+
+if (process.env.node_ENV === 'production') {
+  app.use(express.static('src/build'))
+  app.get('*', () => (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 3001
 
