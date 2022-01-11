7a775c7ef32e67078b58395658a6586bbd89d292
Erin <erin.eliz.costello@gmail.com>
2022-01-11 13:45:52 -0700

    mode cors credentials include

--- src/API/signUpUser.js
+++ src/API/signUpUser.js
@@ -1,6 +1,8 @@
 export const signUpUser = (username, password) => {
   return fetch('https://quizmaker-backend.herokuapp.com/signUp', {
     method: 'POST',
+    mode: 'cors',
+    credentials: 'include',
     body: JSON.stringify({
       username,
       password
@@ -9,5 +11,5 @@ export const signUpUser = (username, password) => {
       'Content-Type': 'application/json',
     }
   })
-    .then(response => response.json()) 
+    .then(response => response.json())
 }
